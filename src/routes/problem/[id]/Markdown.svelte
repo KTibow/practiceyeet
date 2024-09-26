817d4ec05f18e4cb221f9c31fdcb513b4c7c8534
KTibow <KTibow@users.noreply.github.com>
2024-09-25 18:06:29 -0700

    its TeXing time

--- src/routes/problem/[id]/Markdown.svelte
+++ src/routes/problem/[id]/Markdown.svelte
@@ -4,6 +4,52 @@
   const escape = (text: string) =>
     text.replace(/&/g, "&amp;").replace(/</g, "&lt;").replace(/>/g, "&gt;");
 
+  const latexParse = (latex: string) => {
+    let html = latex;
+    const texts: string[] = [];
+
+    // Remove text to prevent formatting
+    html = html.replace(/\\text\{(.+?)\}/g, (match, p1) => {
+      texts.push(p1);
+      return "TEXT" + (texts.length - 1);
+    });
+
+    // Handle fractions
+    html = html.replace(
+      /\\frac\{(.+?)\}\{(.+?)\}/g,
+      '<span class="frac"><span>$1</span><span class="bottom">$2</span></span>',
+    );
+
+    // Handle square roots
+    html = html.replace(
+      /\\sqrt\{([^}]+)\}/g,
+      '<span class="sqrt"><span class="sqrt-stem">$1</span></span>',
+    );
+
+    // Handle superscripts
+    html = html
+      .replace(/\^(\{[^}]+\}|[^\s])/g, "<sup>$1</sup>")
+      .replace(/\{|\}/g, "");
+
+    // Handle subscripts
+    html = html
+      .replace(/_(\{[^}]+\}|[^\s])/g, "<sub>$1</sub>")
+      .replace(/\{|\}/g, "");
+
+    // Handle plus-minus sign
+    html = html.replace(/\\pm/g, '<span class="pm">±</span>');
+
+    // Remove extra backslashes
+    html = html.replace(/\\/g, "");
+
+    // Restore text
+    html = html.replace(/TEXT(\d+)/g, (match, p1) => texts[+p1]);
+
+    // Wrap in math class
+    html = '<span class="math">' + html + "</span>";
+
+    return html;
+  };
   const parse = (text: string) => {
     text = text
       .trim()
@@ -36,6 +82,12 @@
         line = line.replace(/`(.+?)`/g, "<code>$1</code>");
         line = line.replace(/\*\*(.*?)\*\*/g, "<strong>$1</strong>");
         line = line.replace(/\*(.*?)\*/g, "<em>$1</em>");
+        line = line.replace(/\\\((.+?)\\\)/g, (_, n1) => {
+          return latexParse(n1);
+        });
+        line = line.replace(/\\\[(.+?)\\\]/g, (_, n1) => {
+          return latexParse(n1);
+        });
         final += `<p>${line}</p>`;
       }
     }
@@ -74,5 +126,27 @@
     background-color: rgba(var(--m3-scheme-surface-container-highest), 0.5);
     padding: 0.2rem 0.4rem;
     border-radius: 0.25rem;
+  }
+  div :global(.math) {
+    font-family: "Times New Roman", Times, serif;
+  }
+  div :global(.sqrt::before) {
+    content: "√";
+    color: rgb(var(--m3-scheme-outline) / 0.8);
+  }
+  div :global(.sqrt-stem) {
+    border-top: solid 1px rgb(var(--m3-scheme-outline) / 0.8);
+  }
+  div :global(.frac) {
+    display: inline-block;
+    vertical-align: middle;
+    text-align: center;
+  }
+  div :global(.frac > span) {
+    display: block;
+    padding: 0.1rem;
+  }
+  div :global(.frac > span.bottom) {
+    border-top: solid 1px rgb(var(--m3-scheme-outline) / 0.8);
   }
 </style>
