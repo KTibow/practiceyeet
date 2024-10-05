071d24fc5ad1956717e83de976c824e389e142e2
KTibow <KTibow@users.noreply.github.com>
2024-10-05 14:54:59 -0700

    fix reversetemplate for simple case

--- src/lib/reverseTemplate.ts
+++ src/lib/reverseTemplate.ts
@@ -2,5 +2,5 @@ export const reverseTemplate = (template: string, str: string) => {
   const parts = template.split("{{INPUT}}");
   const start = parts[0].length;
   const end = -parts[1].length;
-  return str.slice(start, end);
+  return str.slice(start, end || undefined);
 };
